6dd11671cb3dfeed728b19bc80fa6cd970d92dca
lakshmi096 <lakshmirk.096@gmail.com>
2021-02-13 20:02:12 +0530

    create react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Its Lakshmi's Kitchen</h1>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <section>
+      <p>
+        Get Delicious Receipes
+      </p>
+    </section>
+  )
+}
+
+function Footer() {
+  return(
+    <footer>
+      <p>&copy;Just Kidding</p>
+    </footer>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
