1ecbf1532ee99d5ebffb22308cf928daf961b2c7
lakshmi096 <lakshmirk.096@gmail.com>
2021-02-13 23:31:42 +0530

    array destructuring & conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,61 +1,26 @@
 import './App.css';
 import restaurant from "./assets/restaurant.jpg"
 
-function Header(props) {
-  return (
-    <header>
-      <h1>Its {props.name}'s Kitchen</h1>
-    </header>
-  )
-}
-
-function Main(props) {
-  return (
-    <section>
-      <p>
-        Get {props.description} Receipes
-      </p>
-      <ul>
-          {props.dishes.map(dish => 
-            <li key={dish.id} style={{listStyle:'none', lineHeight:'2em'}}>
-              {dish.title}
-            </li>
-          )}
-        </ul>
-    </section>
-  )
-}
-
-function Footer(props) {
+function SecretComponent() {
   return(
-    <footer>
-      <p>&copy;{props.year}  Just Kidding</p>
-    </footer>
+    <h1>Secret information</h1>
   )
 }
 
-function App() {
-  const dishes = [
-    "Puri Sabzi",
-    "Masala Dosa",
-    "Rajma Chawal",
-    "Dhokla"
-  ]
-  const dishesObject = dishes
-  .map((dish, i) => ({
-      id: i,
-      title: dish
-    })
+function PublicComponent() {
+  return(
+    <h1>Public information</h1>
   )
+}
+
+
+function App({authorized}) {
+  const [,,thirdItem] = ["boots", "tents", "hatcamps"]
   return (
-    <div className="App">
-      <>
-        <Header name="Lakshmi"/>
-        <img src={restaurant} alt="a plate full of yummy dishes" height="200"/>
-      </>
-      <Main description="delicious" dishes={dishesObject} />
-      <Footer year={new Date().getFullYear()}/>
-    </div>
+    <>
+      {authorized? <SecretComponent/> : <PublicComponent/>}
+      <p>Array Destructing: Third Item - {thirdItem}</p>
+    </>
   );
 }
 
